ad4e9b5eedcd24d9e1fbc1bdd3490bd9c9e5ff2a
BeverlyKate <129060018+BeverlyKate@users.noreply.github.com>
2023-06-24 14:16:21 +0800

    Update AStar.java
    Tested and works with the sample graph in main. Final graphs to be used have yet to be coded, but you can use the main.java file for reference.

--- src/AStar.java
+++ src/AStar.java
@@ -1,54 +1,58 @@
 import java.util.*;
 
 public class AStar {
-    public PriorityQueue<Node> visitedNodes = new PriorityQueue<>(); //Evaluated Nodes 
-    public PriorityQueue<Node> toEvaluate = new PriorityQueue<>(); 
+    
+    public static ArrayList<String> Search(Node start, Node goal) {
+    PriorityQueue<Node> visitedNodes = new PriorityQueue<>(); // Evaluated Nodes
+    PriorityQueue<Node> toEvaluate = new PriorityQueue<>();
 
-    public ArrayList<Node> Search (Node start, Node goal){
-        ArrayList<Node> Path = new ArrayList<>();
-        Node node;
+    ArrayList<String> path = new ArrayList<>();
+    Node node = start;
 
-        toEvaluate.add(start);
-        start.f= start.g + start.getHeuristic();
+    toEvaluate.add(start);
+    start.f = start.g + start.getHeuristic();
 
-        while (toEvaluate.size() != 0){
-            node= toEvaluate.peek(); 
-            if (node == goal)
-                Path.add(node);
-            return Path;
-        }  
+    while (!toEvaluate.isEmpty()) {
+        node = toEvaluate.poll();
+        if (node == goal) {
+            break; // Found the goal node, exit the loop
+        }
 
-        for(int i= 0; i < node.neighbors.size(); i++){
-            Node.Edge edge = node.neighbors.get(i);
+        for (Node.Edge edge : node.neighbors) {
             Node neighbor = edge.node;
             double totalCost = node.g + edge.cost;
-            
-            if(!toEvaluate.contains(neighbor) && !visitedNodes.contains(neighbor)){
-                neighbor.parent= node;
-                neighbor.g= totalCost;
-                neighbor.f= neighbor.g + neighbor.getHeuristic();
+
+            if (!toEvaluate.contains(neighbor) && !visitedNodes.contains(neighbor)) {
+                neighbor.parent = node;
+                neighbor.g = totalCost;
+                neighbor.f = neighbor.g + neighbor.getHeuristic();
                 toEvaluate.add(neighbor);
-            }else { //the neighbor node has been visited before 
-
-                if(totalCost < neighbor.g){ //if ne
+            } else { // The neighbor node has been visited before
+                if (totalCost < neighbor.g) {
                     neighbor.parent= node;
-                    neighbor.g= totalCost;
-                    neighbor.f= neighbor.g + neighbor.getHeuristic();
+                    neighbor.g = totalCost;
+                    neighbor.f = neighbor.g + neighbor.getHeuristic();
 
-                    if(visitedNodes.contains(neighbor)){
+                    if (visitedNodes.contains(neighbor)) {
                         visitedNodes.remove(neighbor);
                         toEvaluate.add(neighbor);
                     }
-
                 }
             }
-            toEvaluate.remove(node);
-            visitedNodes.add(node);
-
         }
+        
+        visitedNodes.add(node);
+    }
 
+    // Reconstruct the path if the goal node was found
+    if (node == goal) {
+        while (node != null) {
+            path.add(0, node.name);
+            node = node.parent;
+        }
+    }
 
-        return Path;
-    } 
+    return path;
+}
 
 }
